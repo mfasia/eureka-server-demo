810ec5d302869c6b6c5318ba0b25d7ca01568977
Nadim Jahangir <nadim.jahangir@metafour.com>
2016-02-18 16:31:18 +0600

    first commit

---
+++ src/main/java/com/metafour/eurekaserverdemo/App.java
@@ -0,0 +1,18 @@
+package com.metafour.eurekaserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
